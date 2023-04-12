570171b4eb16c2ff50f8b63579469c9bd4b4d43c
JulienV5ESIEE <julien.sarrau@edu.esiee.fr>
2023-04-12 15:13:29 +0200

    premieres routes

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -8,10 +8,17 @@ app = Flask(__name__)
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
+@app.route('/') ## La route
+def hello_world(): ## Son nom
 	x = 'world'
-	return f"Hello, {x}"
+	return f"Hello, {x}" # Son retour
+
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run() # DOIT TOUJOURS ETRE A LA FIN
+    
